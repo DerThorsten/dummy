a965d8d9b4f1830cff7152db8fea6d32bc7d4136
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2018-02-24 17:21:58 +0100

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,8 +18,8 @@ import sphinx_rtd_theme
 if True:
 
     cmake_defs = "-DDOWNLOAD_DOCTEST=OFF -DBUILD_TEST=OFF  -DBUILD_EXAMPLES=OFF -DDOWNLOAD_GOOGLE_BENCHMARK=OFF -DBUILD_BENCHMARK=OFF"
-
-    subprocess.call('cd ../.. && cmake . %s && make -j2'%(cmake_defs),          shell=True)
+    cmake_py_ver = "-DPYTHON_EXECUTABLE=$HOME/conda/latest/bin/python"
+    subprocess.call('cd ../.. && cmake . %s %s && make -j2'%(cmake_defs, cmake_py_ver),          shell=True)
 
     #subprocess.call('cd ../.. && make install .',   shell=True)
 
